0992a1e39f67468dd0993f282efacb3212f26ef5
hedjouj <messaoui.elhadj@hotmail.fr>
2025-09-22 11:03:07 +0200

    fix: remove an doublon line and added a if statement

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,10 +43,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
-		(*head)->prev->next = (*head)->next;
+		if ((*head)->prev)
+			(*head)->prev->next = (*head)->next;
 		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
 			(*head)->next->prev = (*head)->prev;
 		free(*head);
 		*head = saved_head;
